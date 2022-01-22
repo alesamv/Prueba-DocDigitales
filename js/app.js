7056d605480a6a5d14663f6284899b0f16cd203b
alesamv <alesamv@gmail.com>
2022-01-21 18:56:53 -0600

    x added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Function to give functionality to the modal, when the modal is open the body is freezing
 if(document.getElementById("showModal")){
     var modal = document.getElementById("myModal");
     var btn = document.getElementById("showModal");
@@ -6,17 +7,26 @@ if(document.getElementById("showModal")){
 
     btn.onclick = function() {
         modal.style.display = "block";
-
-        // body.style.position = "static";
+        body.style.position = "static";
         body.style.height = "100%";
-        // body.style.overflow = "hidden";
+        body.style.overflow = "hidden";
     }
 
+    //To close the modal clicking the x
     span.onclick = function() {
         modal.style.display = "none";
-
         body.style.position = "inherit";
         body.style.height = "auto";
         body.style.overflow = "visible";
     }
+
+    //To close the modal without clicking the body
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            body.style.position = "inherit";
+            body.style.height = "auto";
+            body.style.overflow = "visible";
+      }
+  }
 }
